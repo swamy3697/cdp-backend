bad35fd1231fca660f22cf87192bf1e2fd3e8977
swamy3697 <swamyp2002@gmail.com>
2025-02-28 14:30:18 +0530

    updated port

--- app.py
+++ app.py
@@ -457,5 +457,6 @@ def status():
     
     return jsonify({'status': 'success', 'data': status})
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a dynamic port
+    app.run(host="0.0.0.0", port=port)
